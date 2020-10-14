1bf10a9377e5600f2a6d688e96c2aab84f684b7c
marmcrae <mcrae.marquie@gmail.com>
2020-10-14 13:37:54 -1000

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Work and DEV on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
