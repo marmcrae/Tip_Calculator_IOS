d491c9b670bcda8aff25505e3fd46eb4b608093d
marmcrae <mcrae.marquie@gmail.com>
2020-10-14 14:05:17 -1000

    initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var headingLbl: UILabel!
+    @IBOutlet weak var totalAmtLbl: UILabel!
+    @IBOutlet weak var tipPercentageTxt: UITextField!
+    @IBOutlet weak var billTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        totalAmtLbl.text = ""
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let billTotal = Double(billTxt.text!)!
+        let tipPercentage = Double(tipPercentageTxt.text!)! / 100
+        
+        let totalTipAmount = billTotal * tipPercentage
+        totalAmtLbl.text = "$\(totalTipAmount)"
+        
+        
+    }
+    
 }
 
